0622dc98eacb04e1baeb23fd8e67ee0344cbc0ff
VARUN MYNENI <74002498+mynenivarun@users.noreply.github.com>
2023-01-20 17:05:18 +0530

    Create halt.c

---
+++ halt.c
@@ -0,0 +1,15 @@
+Halt
+1.Shutdown.c
+#include "types.h"
+#include "user.h"
+int main(void)
+{
+ halt();
+ exit();
+}
+
+2.Clear.c
+#include "types.h"
+#include "user.h"
+void clear(int x){if(x=='x')return;printf(1,"\xa");
+clear(x+('1'-48));}int main(void){clear('A');exit();}
